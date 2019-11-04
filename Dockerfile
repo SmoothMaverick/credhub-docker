d0890c187c3ba6f085aa03075cadf49eeee78e00
Jon Alvarado <jon.alvarado@newcontext.com>
2019-11-04 09:59:51 -0600

    Using java jar file and rebuilding keys on each run.

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,8 @@ WORKDIR /usr/src/credhub
 RUN "./scripts/setup_dev_mtls.sh"
 RUN ["./gradlew", "--no-daemon", "assemble"]
 
+RUN /bin/mkdir -p /usr/src/credhub/src/test/resources/
+RUN cp ./applications/credhub-api/src/test/resources/auth_server_trust_store.jks /usr/src/credhub/src/test/resources/
+
 COPY ./entrypoint.sh /usr/src/credhub/entrypoint.sh
 ENTRYPOINT ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+#  build a new cert
+./scripts/setup_dev_mtls.sh
+
 profile_uaa=""
 
 if [ "x$UAA_URL" != "x" ]; then
@@ -16,10 +19,12 @@ EOF
 
 fi
 
-exec ./gradlew --no-daemon bootRun \
- -Djava.security.egd=file:/dev/urandom \
- -Djdk.tls.ephemeralDHKeySize=3072 \
- -Djdk.tls.namedGroups='secp384r1' \
- -Djavax.net.ssl.trustStore=src/test/resources/auth_server_trust_store.jks \
- -Djavax.net.ssl.trustStorePassword=changeit \
- -Dspring.profiles.active=dev,dev-h2${profile_uaa} "$@"
+java \
+  -Djava.security.egd=file:/dev/urandom \
+  -Dspring.profiles.active=dev,dev-h2 \
+  -Djavax.net.ssl.trustStore=src/test/resources/auth_server_trust_store.jks \
+  -Djavax.net.ssl.trustStorePassword=changeit \
+  -Djdk.tls.ephemeralDHKeySize=4096 \
+  -Djdk.tls.namedGroups="secp384r1" \
+  -Djava.security.egd=file:/dev/urandom \
+  -jar ./applications/credhub-api/build/libs/credhub.jar "$@"
